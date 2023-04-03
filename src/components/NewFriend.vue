d201874074cb75e2e1d4d91c8a730107ac840a13
Eric Muganga <mugangaeric2020@gmail.com>
2023-04-03 23:58:04 +0200

    Add files via upload

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,39 @@
+<template>
+    <form @submit.prevent="submitData" >
+        <div>
+            <label>Name</label>
+            <input type="text" v-model="enteredName">
+        </div>
+        <div>
+            <label>Phone</label>
+            <input type="tell" v-model="enteredPhone">
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="email" v-model="enteredEmail">
+        </div>
+        <div>
+            <button>Add Friend</button>
+        </div>
+    </form>
+
+</template>
+
+<script>
+export default{
+emits: ['add-friend'],
+data(){
+    return{
+        enteredName: '',
+        enteredPhone: '',
+        enteredEmail: ''
+    }
+},
+methods: {
+    submitData(){
+        this.$emit('add-friend', this.enteredName, this.enteredPhone, this.enteredEmail);
+    }
+}
+};
+
+</script>
